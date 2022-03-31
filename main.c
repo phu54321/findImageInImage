945c9698cf038b3b6092d1ff888c3fcdf549d225
박현우 <phu54321@naver.com>
2022-03-31 20:23:34 +0900

    fast 2d matcher

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 const uint32_t haystack[10][10] = {
     {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
@@ -58,7 +59,8 @@ void calculateRollingHash(
   size_t i;
 
   // Calculate iniial hash for input[0 ~ inputSize - 1]
-  for ( i = 0 ; i < windowSize ; i++) {
+  for (i = 0; i < windowSize; i++)
+  {
     hash = (hash * hashMultiplier) + *input;
     input += inputPitch;
   }
@@ -70,7 +72,8 @@ void calculateRollingHash(
   output += outputPitch;
 
   // Calculate output for the rest of the range.
-  for ( i = windowSize ; i < inputSize ; i++) {
+  for (i = windowSize; i < inputSize; i++)
+  {
     hash = (hash * hashMultiplier) + *input;
     input += inputPitch;
 
@@ -81,36 +84,100 @@ void calculateRollingHash(
   }
 }
 
+void find2DImageInImage(
+    const uint32_t *haystackImage,
+    size_t haystackW,
+    size_t haystackH,
+    size_t haystackPitch,
+
+    const uint32_t *needleImage,
+    size_t needleW,
+    size_t needleH,
+    size_t needlePitch,
+
+    void (*callback)(size_t x, size_t y))
+{
+  // assert(needleH <= haystackH);
+  // assert(needleW <= haystackW);
+
+  size_t searchSpaceW = haystackW - needleW + 1;
+  size_t searchSpaceH = haystackH - needleH + 1;
+
+  uint32_t *hashBuffer = (uint32_t *)malloc(searchSpaceW * haystackH);
+  uint32_t searchHash;
+  size_t x, y;
+
+  // 1) calculate hash of needle
+  for (y = 0; y < needleH; y++)
+  {
+    calculateRollingHash(
+        needleW, hashXMultiplier,
+        needleImage + needlePitch * y, 1, needleW,
+        hashBuffer + searchSpaceW * y, 1);
+  }
+
+  calculateRollingHash(
+      needleH, hashYMultiplier,
+      hashBuffer, searchSpaceW, needleH,
+      &searchHash, 1);
+
+  // 2) calculate rolling hash of haystacks
+  for (y = 0; y < haystackH; y++)
+  {
+    calculateRollingHash(
+        needleW, hashXMultiplier,
+        haystackImage + haystackPitch * y, 1, haystackW,
+        hashBuffer + searchSpaceW * y, 1);
+  }
+
+  for (x = 0; x < searchSpaceW; x++)
+  {
+    calculateRollingHash(
+        needleH, hashYMultiplier,
+        hashBuffer + x, searchSpaceW, haystackH,
+        hashBuffer + x, searchSpaceW);
+  }
+
+  // 3) Search within
+  const uint32_t *hashSearcher = hashBuffer;
+  for (y = 0; y < searchSpaceH; y++)
+  {
+    for (x = 0; x < searchSpaceW; x++)
+    {
+      if (*hashSearcher == searchHash)
+      {
+        // Hash matches. Perform memcmp
+        size_t ix, iy;
+        int exactMatch = 1;
+        for (iy = 0 ; exactMatch && iy < needleH ; iy++) {
+          for(ix = 0 ; ix < needleW ; ix++) {
+            if (needleImage[iy * needlePitch + ix] != haystackImage[(y + iy) * haystackPitch + (x + ix)]) {
+              exactMatch = 0;
+              break;
+            }
+          }
+        }
+        if (exactMatch) {
+          callback(x, y);
+        }
+      }
+      hashSearcher++;
+    }
+  }
+
+  free(hashBuffer);
+}
+
+int cb(size_t x, size_t y) {
+  printf("found (%lu, %lu)\n", x, y);
+}
+
 int main()
 {
-  uint32_t xHashes[2];
-  calculateRollingHash(3, hashXMultiplier, needle[0], 1, 3, &xHashes[0], 1);
-  calculateRollingHash(3, hashXMultiplier, needle[1], 1, 3, &xHashes[1], 1);
-
-  uint32_t searchHash;
-  calculateRollingHash(2, hashYMultiplier, xHashes, 1, 2, &searchHash, 1);
-
-  // hash should be 0x892E8B32
-  printf("search: %08X\n", searchHash);
-
-  uint32_t haystackHashMap[10][8] = {0};
-  for (int y = 0 ; y < 10 ; y++) {
-    calculateRollingHash(3, hashXMultiplier, haystack[y], 1, 10, haystackHashMap[y], 1);
-  }
-
-  for (int x = 0 ; x < 8 ; x++) {
-    calculateRollingHash(2, hashYMultiplier, &haystackHashMap[0][x], 8, 10, &haystackHashMap[0][x], 8);
-  }
-
-  for (int y = 0 ; y < 9 ; y++) {
-    for(int x = 0 ; x < 8 ; x ++) {
-      printf("%08X  ", haystackHashMap[y][x]);
-      if (haystackHashMap[y][x] == searchHash) {
-        printf("\nfound: (%d, %d)\n", x, y);
-      }
-    }
-    printf("\n");
-  }
+  find2DImageInImage(
+      &haystack[0][0], 10, 10, 10,
+      &needle[0][0], 3, 2, 3,
+      cb);
 
   return 0;
 }
