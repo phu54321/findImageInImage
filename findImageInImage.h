88e27b13d425923e2abfa7fb6eacdc06b16868de
박현우 <phu54321@naver.com>
2022-04-07 20:23:09 +0900

    feat: support for c++

--- findImageInImage.h
+++ findImageInImage.h
@@ -12,17 +12,25 @@
 
 #include <stdint.h>
 
-void find2DImageInImage(
-    const uint32_t *haystackImage,
-    size_t haystackW,
-    size_t haystackH,
-    size_t haystackPitch,
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+    void find2DImageInImage(
+        const uint32_t *haystackImage,
+        size_t haystackW,
+        size_t haystackH,
+        size_t haystackPitch,
 
-    const uint32_t *needleImage,
-    size_t needleW,
-    size_t needleH,
-    size_t needlePitch,
+        const uint32_t *needleImage,
+        size_t needleW,
+        size_t needleH,
+        size_t needlePitch,
 
-    void (*callback)(size_t x, size_t y));
+        void (*callback)(size_t x, size_t y));
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
